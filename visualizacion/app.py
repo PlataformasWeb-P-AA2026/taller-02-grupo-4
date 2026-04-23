77ad1a40aaec4bb189e1aaea672422eed0386136
astalberto <albertoherrera205@hotmail.com>
2026-04-23 10:24:58 -0500

    Correcion en app.py

--- visualizacion/app.py
+++ visualizacion/app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import requests
 import pandas as pd
+from config import API_URL
 
 st.title("Dashboard")
 
-data = requests.get("http://127.0.0.1:8000/empleados").json()
+data = requests.get(API_URL).json()
 df = pd.DataFrame(data)
 
 st.dataframe(df)
